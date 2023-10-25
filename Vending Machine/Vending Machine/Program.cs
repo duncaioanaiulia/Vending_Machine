388aef66909b2647c355ee0ae885afc8451688d9
dunca <dunca@IOANA>
2023-10-25 18:12:55 +0300

    Finish the program

--- Vending Machine/Vending Machine/Program.cs
+++ Vending Machine/Vending Machine/Program.cs
@@ -1,57 +1,87 @@
 ﻿using System;
 using System.Globalization;
 
+// Intrebare :  De ce cand am lucrat cu double am avut erroare de scadere ? 
+// La varianta :  [3, 3, 3.9] la <change> pe double imi dadea  0.090000 ... 
 class Program
 {
-    private static int billAccepted = 10;
+    private const int BillAccepted = 10;
+
+    private const double Coins50 = 0.5;
+    private const double Coins10 = 0.1;
 
     public static void Main()
     {
-        var nums = InputNumebers();
-
-        int sum = nums.Sum();
-
-        var coverValue = CoverValue(sum);
-        
-        if(coverValue == 0)
+        while (true)
         {
-            Console.WriteLine("Error: Please contact admin");
+            Console.WriteLine();
+            Console.WriteLine();
+            VendingMachine();
+        }
+    }
+
+    public static void VendingMachine()
+    {
+        float[] nums = InputNumebers();
+
+        float sumProducts = nums.Sum();
+
+        var coverValue = CoverValue(sumProducts);
+        
+        if(coverValue < 0)
+        {
+            RestartProgram();
             return;
         }
 
+        float maxBillInserted = coverValue * BillAccepted;
+
         do
         {
-            Console.WriteLine(string.Format("Please insert {0} lei coin for {1} time :", billAccepted, coverValue));
-            Console.WriteLine("(Press any key for insert)");
+            Console.WriteLine(string.Format("Please insert {0} lei coin for {1} time :", BillAccepted, coverValue));
+            Console.WriteLine("(Press any key to insert)");
             Console.ReadKey();
             coverValue--;
         }
         while (coverValue > 0);
 
+        Console.WriteLine("Wait for your tips:");
 
-        Console.WriteLine(nums);
+        float change = maxBillInserted - sumProducts;
+
+        int num50Coins = (int) (change / Coins50);
+
+        int num10Coins = (int) ((change- (num50Coins * Coins50)) / Coins10); 
+
+        Console.WriteLine(string.Format("{0:N2}", change) + string.Format(" lei in {0} on 50 coins and {1} in 10 coins", num50Coins, num10Coins));
     }
 
-    private static int[] InputNumebers()
+    private static float[] InputNumebers()
     {
+
         Console.WriteLine("Please add 3 products:");
         string console = Console.ReadLine();
-        string[] tokens = console.Split(" ");
-        IList<int> nums = new List<int>();
-        int oneNum;
+        string[] tokens = console.Split(new char[] { ' ', ','});
+        IList<float> nums = new List<float>();
+        float oneNum;
         foreach (string s in tokens)
         {
-            if (Int32.TryParse(s, out oneNum))
+            if (float.TryParse(s, out oneNum))
                 nums.Add(oneNum);
         }
         return nums.ToArray();
     }
 
-    private static int CoverValue(int sum)
+    private static int CoverValue(float sum)
     {
-        int coverValue = sum / billAccepted;
-        return coverValue > 0 ? coverValue + 1 : 0;
+        float coverValue = sum / BillAccepted;
+        return coverValue > 0 ? (int)coverValue + 1 : 0;
     }
 
-    private bool IsCorrectBill(int value) => value == 10;
+    private static void RestartProgram()
+    {
+        Console.WriteLine("Error: Repeat");
+        Console.WriteLine();
+        InputNumebers();
+    }
 }
